9cf3c1c25b7cef71a0c8ce34783f540c6c045105
Izzat Mahmud <[email protected]>
2026-08-13 18:28:17 +0500

    Fix corrupted package.json

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 ﻿require("dotenv").config();
 const app = require("./app");
 
+// THIS IS THE ONLY CHANGE - USE AZURE'S PORT
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`BingeIt server running at http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`BingeIt server running on port ${PORT}`);
 });
